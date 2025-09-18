a591adfb66b6d7c0dc24006c8d1af62ec85ea5c5
Cliff Chen <cfchen@futuredial.com>
2025-09-18 22:21:22 +0800

    Refactor

--- Merit.lua
+++ Merit.lua
@@ -9,15 +9,17 @@ local packets = require("packets")
 
 require('mylibs/utils')
 require('mylibs/fsd_lite')
+require('mylibs/aggro')
+require('mylibs/trusts')
 
 local enabled = false
-local start = false
-local ws_count = 0
+local fighting = false
 local lastwsCheck = os.clock()
 local lastwarpCheck = os.clock()
+local aggroCheck = os.clock()
 local mpoints = 75
 
-TRUSTS_FARM = {
+TRUSTS = {
     -- "コーネリア",
     "クポフリート",
     "サクラ",
@@ -39,41 +41,20 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
     end
 end)
 
-function action_handler(act)
-    if not enabled then
-        return
-    end
-    local actionpacket = ActionPacket.new(act)
-    local category = actionpacket:get_category_string()
-
-    if category == 'weaponskill_finish' and act.param ~= 0 then
-		log(ws_count)
-		ws_count = ws_count + 1
-		if ws_count >= 2 then
-			windower.send_command('lz stop')
-			windower.send_command('/a off; /a off; /a off; /a off; ')
-			start_fight()
-		end
-	end
-end
-ActionPacket.open_listener(action_handler)
-
 windower.register_event('prerender', function(...)
-    if not enabled then return end
-    
     if warping and (os.clock() - lastwarpCheck) > 3 then
-        if T{298,279}:contains(windower.ffxi.get_info()['zone']) then
-			-- windower.send_command(windower.to_shift_jis('input /ma デジョン <me>'))
+        if T{288}:contains(windower.ffxi.get_info()['zone']) then
+			windower.send_command(windower.to_shift_jis('input /equip ring1 デジョンリング; wait 12; input /item "デジョンリング" <me>'))
         else
             log('Warp completed')
             warping = false
         end
         lastwarpCheck = os.clock()
     end
-    if windower.ffxi.get_player()['status'] == 1 and (os.clock() - lastwsCheck) > 0.8 then
+    if enabled and (os.clock() - lastwsCheck) > 0.8 then
         tp = windower.ffxi.get_player()['vitals']['tp']
         if tp > 999 then
-			if isJob('wAR') then
+			if isJob('WAR') then
 				windower.send_command('input /ws '..windower.to_shift_jis("フェルクリーヴ")..' <t>')
 			elseif isJob('THF') then
 				windower.send_command('input /ws '..windower.to_shift_jis("イオリアンエッジ")..' <t>')
@@ -81,29 +62,75 @@ windower.register_event('prerender', function(...)
         end
         lastwsCheck = os.clock()
     end
+	if fighting and os.clock() - aggroCheck > 1 then
+		if aggroCount() == 0 then
+			aggroCheck = os.clock() + 5
+			main_function(windower.ffxi.get_info()['zone'])
+			return
+		end
+		aggroCheck = os.clock()
+	end
 end)
 
 windower.register_event('status change', function(new, old)
     if new == 2 then
 		enabled = false
-		start = false
     end
 end)
 
-function start_fight()
-	if not enabled or not start then return end
+function main_function(zone)
+	if not enabled then return end
 	
-	ws_count = 0
-	if in_pos(-321.14016723633,345.8117980957,50) then
-		log('go')
-		fsd_go_reverse('mrt', 'mrt_hunt', function()
-			windower.send_command('lz start')
-		end)
+	if zone == 288 then--エスカ-ジ・タ
+		if in_pos(-321.14, 345.81 ,50) then
+			if aggroCount() == 0 then
+				log('go')
+				fighting = false
+				windower.send_command('lz stop')
+				fsd_go_reverse('mrt', 'mrt_hunt', function()
+					main_function(windower.ffxi.get_info()['zone'])
+				end)
+			else
+				log('clean')
+				windower.send_command('lz clean; wait 0.5; lz start')
+				coroutine.sleep(2)
+				fighting = true
+			end
+		elseif in_pos(-236, 619.97, 50) then
+			if aggroCount() == 0 then
+				log('go back')
+				fighting = false
+				windower.send_command('lz stop')
+				fsd_go('mrt', 'mrt_hunt', function()
+					main_function(windower.ffxi.get_info()['zone'])
+				end)
+			else
+				log('clean')
+				windower.send_command('lz clean; wait 0.5; lz start')
+				coroutine.sleep(2)
+				fighting = true
+			end
+		elseif in_pos(-345.42, -178) then
+			fsd_go('mrt', 'zone_ez_sT1', function()
+				windower.send_command('sw ew 2')
+				coroutine.sleep(5)
+				callnpc('mrt', function()
+					main_function(windower.ffxi.get_info()['zone'])
+				end)
+			end)
+		end
+		
+    elseif zone == 126 then--クフィム島
+		fsd_go('mrt', 'zone_q_hpTesc', function()
+			windower.send_command('sw ew enter')
+        end)
+	
 	else
-		log('bak')
-		fsd_go('mrt', 'mrt_hunt', function()
-			windower.send_command('lz start')
-		end)
+		if isNpcNear('Home Point') then
+			windower.send_command('sw hp island')
+		else
+			log('No Home point nearby...')
+		end
 	end
 end
 
@@ -112,54 +139,34 @@ windower.register_event('zone change', function(new, old)
     if not enabled then return end
     
 	coroutine.sleep(10)
-    if new == 288 then--エスカ-ジ・タ
-		fsd_go('mrt', 'zone_ez_sT1', function()--todo, path
-			windower.send_command('input //sw ew 2')
-			coroutine.sleep(5)
-            for c = 1, #TRUSTS_FARM do
-                windower.send_command(windower.to_shift_jis('input /ma "'..TRUSTS_FARM[c]..'" <me>'))
-                coroutine.sleep(6)
-            end
-			start = true
-			start_fight()
-		end)
-        
-    elseif new == 126 then--クフィム島
-		fsd_go('mrt', 'zone_q_hpTesc', function()--todo, path
-			windower.send_command('input //sw ew enter')
-        end)
-    end
+    main_function(new)
 end)
 
 windower.register_event('addon command', function(command, ...)
     if T{"go","g","gg"}:contains(command) then
         enabled = true
-        start = false
+		windower.send_command('lr combat')
+		main_function(windower.ffxi.get_info()['zone'])
 		
-    elseif T{"test"}:contains(command) then
-        enabled = true
-		start = true
-		start_fight()
-	
     elseif T{"stop","s"}:contains(command) then
         enabled = false
-		start = false
-        windower.send_command("input //fsd s")
+        windower.send_command("lz s")
+        windower.send_command("fsd s")
 
     end
 end)
 
 windower.register_event('load', function()
     log('===========loaded===========')
-	if isJob('wAR') then
+	if isJob('WAR') then
 		windower.send_command("gc gax")
 		windower.send_command("ata off")
 	elseif isJob('THF') then
 		windower.send_command("gc aby; wait 1; gc su5")
 	end
-    windower.send_command("input //ws s")
+    windower.send_command("ws s")
 end)
 
 windower.register_event('unload', function()
-    windower.send_command("input //fsd s")
+    windower.send_command("fsd s")
 end)
